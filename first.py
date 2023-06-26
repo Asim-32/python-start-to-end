8ed2b8e3de77f049038bf57953c5fc89231b2d42
Manas  Raj <37516396+Manasraj1990@users.noreply.github.com>
2023-06-26 23:53:33 +0530

    first python notes

---
+++ first.py
@@ -0,0 +1,34 @@
+"""
+author: Manas 
+Use: This is my first python class
+and I will be a python champion.
+"""
+#indentation 
+def manas():
+    print("manas")    
+manas()
+if(1==1):
+    print("Hello world")
+
+x = str(3)
+X = "name"
+
+print(type(x), type(X))
+
+x, y, z = 10,20,"Asim"
+
+print(x, y, z)
+
+
+#List
+fruits = ["apple", "banana", "cherry"]
+
+q, w, e = fruits
+print(q)
+print(w)
+print(e)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(f'x = {x}, y = {y}, z = {z}')
