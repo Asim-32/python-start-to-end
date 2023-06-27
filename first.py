389c97451bd57110f0a1f4d13178454cb24df33f
Manas  Raj <37516396+Manasraj1990@users.noreply.github.com>
2023-06-27 18:27:40 +0530

    update

--- first.py
+++ first.py
@@ -32,3 +32,20 @@ x = "Python "
 y = "is "
 z = "awesome"
 print(f'x = {x}, y = {y}, z = {z}')
+
+
+#while loop
+"""
+i = 6/0
+while i<10:
+    print(i)
+    i += 1"""
+
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+    
---
+++ list.py
@@ -0,0 +1,17 @@
+username = ['manas','asim','heera','adesh',1,2,3,"8u",687]
+
+# print(len(username))
+# print(username[2])
+
+# for i in username:
+#     print(i)
+
+# username.pop()
+# print(username)
+
+# inx = username.index('asim')
+# print(inx)
+
+print(username)
+print(username[2:])
+print(username[-2:])
